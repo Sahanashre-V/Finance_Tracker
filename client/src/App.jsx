a7d8d02ce21beeee216cc81ea45db2b99fc164fc
Sahanashre-V <sahanashre.v@gmail.com>
2025-08-30 21:17:14 +0530

    Added both frontend and backend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+function App() {
+
+  return (
+    <>
+    <p className="text-orange-400">Welcome to Finance Tracker</p>
+    </>
+  )
+}
+
+export default App
